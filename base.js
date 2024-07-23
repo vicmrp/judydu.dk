a61b14f865ec131e1c10de46bab46aff735994b4
Victor Reipur <victor.reipur@gmail.com>
2024-07-23 22:21:24 +0200

    neweset commit

--- base.js
+++ base.js
@@ -2,16 +2,23 @@ console.log("Base script loaded successfully");
 
 
 
-$(document).ready(function() {
-    const includeHTML = (selector, url) => {
-        $(selector).load(url, function(response, status, xhr) {
-            if (status == "success") {
-                $(document).trigger('includeContentLoaded');
-            }
-        });
-    };
+// $(document).ready(function() {
+//     const includeHTML = (selector, url) => {
+//         // Return a new Promise
+//         return new Promise((resolve, reject) => {
+//             $(selector).load(url, function(response, status, xhr) {
+//                 if (status == "success") {
+//                     $(document).trigger('includeContentLoaded');
+//                     resolve(); // Resolve the promise on success
+//                 } else {
+//                     reject(xhr.statusText); // Reject the promise on failure
+//                 }
+//             });
+//         });
+//     };
 
-    includeHTML("head", "/head.html");
-    includeHTML("header", "/header.html");
-    includeHTML("footer", "/footer.html");
-});
+//     // Chain the calls with .then()
+//     includeHTML("header", "/header.html")
+//         .then(() => includeHTML("footer", "/footer.html"))
+//         .catch(error => console.error("Failed to load HTML:", error)); // Catch any error
+// });
--- startpage/script.js
+++ startpage/script.js
@@ -1,8 +1,1 @@
 console.log("Script loaded successfully");
-
-
-// // Listen for the custom event indicating main content is loaded
-// $(document).on('includeContentLoaded', function() {
-//     console.log("Main content loaded successfully");
-//     // Add your custom scripts here
-// });
