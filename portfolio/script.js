40bb7b735f66504f6ed1c714f24e6e9d054303c2
Victor Reipur <victor.reipur@gmail.com>
2024-07-25 20:42:43 +0200

    works and cleaned

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    adjustCarouselHeight();
+    loadAboutUs();
     window.addEventListener('resize', adjustCarouselHeight);
 });
 
+function loadAboutUs() {
+    fetch("about-us.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('about-us-section').innerHTML = data;
+            adjustCarouselHeight();  // Adjust height after content is loaded
+        });
+}
+
 function adjustCarouselHeight() {
-    // Get the main container height
     var mainHeight = document.querySelector('main').clientHeight;
-
-    // Get the height of the About Us section
     var aboutUsHeight = document.getElementById('about-us-section').clientHeight;
-
-    // Calculate the new height for the carousel
     var carouselHeight = mainHeight - aboutUsHeight;
 
-    // Set the height of the carousel
     var carousel = document.querySelector('.carousel-inner');
     if (carousel) {
         carousel.style.height = `${carouselHeight}px`;
